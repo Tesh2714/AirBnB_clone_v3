3820492633f246726797a7f2b232ec9a31986f6d
Tesh2714 <teas38mum@gmail.com>
2022-10-25 03:52:37 -0700

    add

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,5 +23,5 @@ def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
+    app.run(host=os.getenv('CORS', '0.0.0.0'),
+            port=int(os.getenv('CORS', '5000')))
